6b051e4f8d661cc233b4c5779febab108ab6ff15
dawipo-project <71676498+dawipo-project@users.noreply.github.com>
2020-09-22 12:02:16 -0500

    Add initial files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+google.charts.load("current", { packages: ["corechart", "bar"] });
+google.charts.setOnLoadCallback(drawStacked);
+
+function drawStacked() {
+  var data = google.visualization.arrayToDataTable([
+    ["Problema", "Frecuencia", "Intensidad", "Prioridad"],
+    ["Costos Ocultos", 2, 3, 5],
+    ["Información fragmentada", 3, 2, 5],
+    ["Tiempos de abastecimiento", 2, 3, 5],
+    ["Tiempos de reacción lentos", 2, 3, 5],
+    ["Dependen de data externa", 5, 0, 5],
+    ["Consultan muchos archivos", 4, 4, 8],
+    ["Poco acceso a inventario", 4, 4, 8],
+    ["Concentración en otras áreas", 5, 5, 10],
+    ["Muchos procesos manuales", 5, 5, 10],
+  ]);
+
+  var options = {
+    title: "Principales dolores identificados por los entrevistados",
+    chartArea: { width: "50%" },
+    isStacked: true,
+    hAxis: {
+      title: "",
+      minValue: 0,
+    },
+    vAxis: {
+      title: "",
+    },
+  };
+  var chart = new google.visualization.BarChart(
+    document.getElementById("chart_div")
+  );
+  chart.draw(data, options);
+}
