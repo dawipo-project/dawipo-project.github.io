bea6ba9a3c2ef54c0e1eee4fc381eb55e010bea0
dawipo-project <71676498+dawipo-project@users.noreply.github.com>
2020-10-12 00:06:31 -0500

    Add files via upload

---
+++ grafico/main.js
@@ -0,0 +1,42 @@
+// 'Carol García - Home Sentry',
+// 'Juan Miguel Caicedo - HS Colombia',
+// 'Claudia Rubio - Forto',
+// 'Carlos Eduardo Silva - Goby Filters',
+// 'Daniela Espinosa - Nahbu Travel',
+// 'Lina María Aristizábal - Aportapp',
+google.charts.load("current", { packages: ["bar"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Proceso", "Carol García - Home Sentry", "Juan Miguel Caicedo - HS Colombia", 
+    "Claudia Rubio - Forto", "Carlos Eduardo Silva - Goby Filters", "Daniela Espinosa - Nahbu Travel", 
+    "Lina María Aristizábal - Aportapp"],
+    ["Incrementar las ventas", 1, 0, 0, 0, 0, 0],
+    ["Administración de órdenes de compra", 0, 1, 0, 0, 0, 1],
+    ["Programas de fidelización de clientes", 0, 0, 1, 1, 1, 1],
+    ["Optimización de inventario", 1, 1, 0, 0, 0, 1],
+    ["Generación de alertas en cadena de suministro", 0, 1, 0, 0, 1, 1],
+  ]);
+
+  var options = {
+    bars: "horizontal", // Required for Material Bar Charts.
+    height: 500,
+    backgroundColor: '#e9ecef',
+    fontName: 'Open Sans',
+    colors: ["#1b9e77", "#16b558", "#15ab29", "#16b5b2", "#1586ab", "#1a6ead"],
+  };
+
+  var chart = new google.charts.Bar(document.getElementById("chart_div"));
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+
+  var btns = document.getElementById("btn-group");
+
+  btns.onclick = function (e) {
+    if (e.target.tagName === "BUTTON") {
+      options.hAxis.format = e.target.id === "none" ? "" : e.target.id;
+      chart.draw(data, google.charts.Bar.convertOptions(options));
+    }
+  };
+}
